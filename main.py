c7b1a05fd8e0ae2c43dbf0e8c8ee8b82861ae442
Laura <jreqnie2902@iesmarquesdecomares.org>
2026-01-14 11:06:47 +0100

    Modificado 1

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 #   - mostrar_lista(tareas)
 #   - main()
 #
-# IMPORTANTE:
+# IMPORTANTE:Ale tontorron
 #   main() debe contener el bucle principal
 #   que permita al usuario interactuar hasta que elija salir.
 # =================================================
