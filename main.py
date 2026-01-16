f3f95a30ff3a1ffb5988438a40c110d95e8ec105
klok <joligar1811@iesmarquesdecomares.org>
2026-01-16 09:58:44 +0100

    amo ha prova

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 #   - mostrar_lista(tareas)
 #   - main()
 #
-# IMPORTANTE:Madre mia
+# IMPORTANTE: Madre mia
 #   main() debe contener el bucle principal
 #   que permita al usuario interactuar hasta que elija salir.
 # =================================================
